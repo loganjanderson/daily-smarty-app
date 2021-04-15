955bba238535d1f8006d97f5598069b9c7a76465
Logan Anderson <logananderson@Logans-MBP.T-mobile.com>
2021-04-15 10:51:05 -0600

    added in logo

--- src/components/app.js
+++ src/components/app.js
@@ -5,8 +5,6 @@ export default class App extends Component {
   render() {
     return (
       <div className="app">
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
         <Logo />
       </div>
     );
